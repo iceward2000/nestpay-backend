982c9580eedf093c1e07117a6e1bacb55a25ef94
iceward2000 <huseyin.gemici2000@gmail.com>
2025-12-31 15:16:48 +0300

    name change of env again

--- src/config/nestpay.js
+++ src/config/nestpay.js
@@ -6,11 +6,10 @@ export const nestpayConfig = {
     clientId: process.env.PAYTEN_CLIENT_ID,
     storeKey: process.env.PAYTEN_STORE_KEY,
     gatewayUrl: process.env.PAYTEN_3D_URL,
-    baseUrl: process.env.PAYTEN_BASE_URL,
+    baseUrl: process.env.BASE_URL,
     storeType: '3D_PAY_HOSTING',
     lang: 'tr',
     currency: '949', // TRY
     tranType: 'Auth',
-    hashAlgorithm: 'ver3'
   };
   
